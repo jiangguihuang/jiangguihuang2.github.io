c41b27d08363492beb110c81d026b1a61dd606f0
jiangguihuang <105112821+jiangguihuang@users.noreply.github.com>
2023-02-09 23:17:49 +0800

    Add files via upload
    test

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/test.jpg') {
+      myImage.setAttribute('src', 'images/test2.jpg');
+    } else {
+      myImage.setAttribute('src', 'images/test.jpg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('请输入你的名字。');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = '哼哼哼啊，啊啊啊啊，啊啊啊啊啊啊啊啊啊啊啊啊，' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = '哼哼哼啊，啊啊啊啊，啊啊啊啊啊啊啊啊啊啊啊啊，' + storedName;
+}
+
